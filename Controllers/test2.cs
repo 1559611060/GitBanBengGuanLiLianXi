33eb311dec776c4d79c93d0a94760393ab28c086
周平 <1559611060@qq.com>
2020-07-06 16:47:43 +0800

    开发提交1

---
+++ Controllers/test2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitBanBengGuanLiLianXi.Controllers
+{
+    public class test2
+    {
+
+    }
+}
